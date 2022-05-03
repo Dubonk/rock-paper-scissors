80af79e437d2c29c4d3af0e95a3e2802c5b3e606
kevin <kevindubon43@gmail.com>
2022-05-02 18:50:31 -0700

    got most stuff working, game ends at round 5 not winner at round 5.

--- script.js
+++ script.js
@@ -1,78 +1,93 @@
 let computerChoice = randomNum(1,3);
-let computerSelection = computerPlay();
-let c = computerSelection;
+let computerSelection; // = computerPlay();
+let c; //  = computerSelection;
+let p;
+let result;
+let win; 
+let lose; 
 let playerScore = 0;
 let computerScore = 0;
-let draw = 'tie!';
+let draw;
 function randomNum(min, max) {
     return Math.floor(Math.random()* (max - min + 1) + min);
 }
 function computerPlay() {
-    let i = computerChoice;
-    if (i == 1) {
+    let n = randomNum(1, 3);
+
+    if (n == 1) {
         return 'rock';
     }
-    else if (i == 2) {
+    else if (n == 2) {
         return 'paper';
     }
     else {
         return 'scissors';
     }
 }
-
+function playerPlay() {
+    p = prompt("Rock, Paper, or Scissors?").toLowerCase();
+}
 function playRound() {
-    let p = prompt('rock, paper, or scissors?').toLowerCase();
-    let win = `You win! ${p} beats ${c}!`;
-    let lose = `You lose... ${c} beats ${p}`;
-
-    if ( p == 'rock' && c == 'scissors') {
-        playerScore++; 
-        return win; 
+    let c = computerPlay();
+    playerPlay();
+    win = `You win! ${p} beats ${c}!`;
+    lose = `You lose... ${c} beats ${p}`;
+    draw  = 'tie!';
+    if ( p == 'rock' && c == 'scissors') { 
+        result = win; 
+        playerScore++;
+        //console.log(win);
     }
     else if ( p == 'rock' && c == 'paper') {
+        result = lose;
         computerScore++;
-        return lose;
+        //console.log(lose);
     }
     else if ( p == 'paper' && c == 'rock') {
+        result = win;
         playerScore++;
-        return win;
+       // console.log(win);
     }
     else if (p == 'paper' && c == 'scissors') {
+        result = lose;
         computerScore++;
-        return lose;
+        //console.log(lose);
     }
     else if (p == 'scissors' && c == 'paper') {
+        result = win;
         playerScore++;
-        return win;
+        //console.log(win);
+
     }
     else if (p == 'scissors' && c == 'rock') {
+        result = lose;
         computerScore++;
-        return lose;
+        //console.log(lose);
     }
-    else if (p === c) {
-        return draw;
+    else if (p == c) {
+        result = draw;
+        //console.log(draw);
     }
     else {
         alert(`Oops! ${p} is not an option`);
     }
-    
-}
-function endGame() {
-    if (playerScore > 4) {
-        console.log(`Game Over! You Win! Your score was ${playerScore}`);
-    } else if (computerScore > 4) {
-        console.log(`Game Over! You Lost... Your score was ${playerScore}`);
-    }
 }
 function game() {
-    playRound();
-    for (let i = 0; playerScore > 4 || computerScore > 4; i++) {
-    if (playerScore < 5 || computerScore < 5) {
-    	game();
+
+    for (i = 1; i <= 5; i++) {
+        computerPlay();
+        playRound();
+        console.log(`Round # ${i}`);
+        console.log(result);
+        if (i == 5) {
+    if (playerScore > computerScore) {
+        console.log(`You beat the computer ${playerScore} to ${computerScore}!`);
     }
-    else if (playerScore > 4 || computerScore > 4) {
-    	endGame();
-    }}
-}
-game();
-console.log(playRound())
+    else if (playerScore < computerScore) {
+        console.log(`The computer beat you ${computerScore} to ${playerScore}`);
+    }  
+    else if (playerScore == computerScore) {
+        console.log(`It's a tie! ${playerScore} to ${computerScore}`)
+    } 
+    }} }
+game()
